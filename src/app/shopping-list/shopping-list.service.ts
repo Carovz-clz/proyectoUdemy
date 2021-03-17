5ab93d798d7a4a66e6cb61d212584e41ee637b84
Carolina Zapata <cvzapata@minsait.com>
2021-03-17 17:07:08 +0100

    Servicio para la lista de la compra

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,2 +1,19 @@
-export class SoppingListService {
+import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+
+export class ShoppingListService {
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 6),
+        new Ingredient('Tomatoes', 3)
+    ];
+
+    getIngridients(){
+        return this.ingredients.slice();
+    }
+
+    addIngredient(ingredient: Ingredient){
+        this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
 }
